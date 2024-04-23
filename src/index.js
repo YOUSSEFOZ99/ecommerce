9c19ba7dcb89c19611e6c963475ba75a17342b81
pc <chahbiyoussef99@gmail.com>
2024-04-23 13:05:49 +0100

    first

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import 'bootstrap/dist/js/bootstrap.min.js';
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js')
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import '@fortawesome/fontawesome-free/js/all.min.js';
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
