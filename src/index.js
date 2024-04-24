bbecdbf67ab8c507fe0cbd000235e0ce11397201
pc <chahbiyoussef99@gmail.com>
2024-04-24 21:52:09 +0100

    most sold product

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ import '@fortawesome/fontawesome-free/js/all.min.js';
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener("click", () => {
+        alert('اضيف المنتج الى عربة الشراء');
+    })
+});
